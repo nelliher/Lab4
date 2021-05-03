3bfd17495c70ffdd70745343335a076888be0895
nelli her <nellih1117@gmail.com>
2021-05-02 19:51:24 -0700

    finish pseudo code

--- lab_4.py
+++ lab_4.py
@@ -11,5 +11,23 @@ If the user could not guess the correct answer within the max_tries, then print 
 then print "The correct answer is 'Sacramento'".
 """
 
+"""
+main
+  question = "What is the capitol of California"
+  answer = "Sacramento"
+  ask(question, answer)
 
+ask
+  tries = 0
+  loop three times
+    increment tries by 1
+    ask user input()
+    check to see if user input is equal to answer
+      if so, print "Correct" then exit loop
+   if not correct
+    print to the user "You have used up your allotment of guesses."
+    print the correct answer "The correct answer is 'Sacramento'"
+
+main
+"""
 
